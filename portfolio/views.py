15071b33b663a2da34fca488c0f4dadae80071a3
Dallas <69281311+pataskad@users.noreply.github.com>
2021-02-09 09:49:35 -0600

    peeled email settings to start fresh

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from .models import *
-from .forms import contact_form
 from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import redirect
 
 def index(request):
-    if request.method == 'GET':
-        form = contact_form()
-    else:
-        form = contact_form(request.POST)
-        if form.is_valid():
-            contact_name = form.cleaned_data['contact_name']
-            contact_email = form.cleaned_data['contact_email']
-            contact_message = form.cleaned_data['contact_message']
-            try:
-                send_mail(contact_name, contact_message, contact_email, ['pataskad@pixbyjade.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect('success')
-    return render(request, 'index.html', {'form': form})
+   
+    return render(request, 'index.html')
 
-def success(request):
-    return HttpResponse('Success! Thank you for your message.')
+
