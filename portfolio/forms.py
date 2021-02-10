b423617298a57aa127c8eab2f0a94094ab259ecf
Dallas <69281311+pataskad@users.noreply.github.com>
2021-02-10 17:48:40 -0600

    contact form backend progress

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
+
 class contactForm(forms.Form):
     email=forms.EmailField(required=True)
     name=forms.CharField(max_length=100, required=True)
-    message=forms.CharField(widget=forms.Textarea,required=True)
+    message=forms.CharField(widget=forms.Textarea, required=True)
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,20 +4,29 @@ from django.conf import settings
 from portfolio.forms import contactForm
 
 
+
 def index(request):
     if request.method == 'GET':
         form_class = contactForm
         form = form_class(None)
-        return render(request, 'index.html',  {'form':form})
+        context = {
+                'form': form,
+            }
+        return render(request, 'index.html', context)
     if request.method == 'POST':
+        
         form=contactForm(request.POST)
         if form.is_valid():
             email=form.cleaned_data['email']
             name=form.cleaned_data['name']
             message=form.cleaned_data['message']
-
+    
             send_mail(name, message, email, ['nelsonj@pixbyjade.com'], fail_silently=False)
+
         else:
             form = contactForm()
-    return render(request, 'index.html', {'form':form})
+            context = {
+                'form': form,
+            }
+    return redirect('/')
 
