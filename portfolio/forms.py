4221495a84df16f336656cd12b5caf5c7702db40
Dallas <69281311+pataskad@users.noreply.github.com>
2021-02-19 15:00:53 -0600

    last design changes pre 1.0, final contact from front end

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 
 
+
+#this contact form is not being used for anything, keeping it here for the time being just in case as of 02/19/21
 class contactForm(forms.Form):
     name = forms.CharField(required=True, max_length=100)
     email_address = forms.EmailField(max_length=150)
     message = forms.CharField(required=True, widget=forms.Textarea, max_length=1000)
+
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,35 +1,21 @@
 from django.shortcuts import render, redirect
-from .forms import contactForm
 from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib import messages
-from django.core.mail import send_mail, BadHeaderError
+from django.core.mail import send_mail
 
 
 
 def index(request):
-    if request.method == 'POST':
-        form = contactForm(request.POST)
-        if form.is_valid():
-            subject = "Contact Request"
-            body = {
-                'name': form.cleaned_data['name'],
-                'email': form.cleaned_data['email_address'],
-                'message': form.cleaned_data['message'],
-            }
-            message = "\n".join(body.values())
-
-            try:
-                send_mail(subject, message, 'pixbyjadecontact@gmail.com', ['nelsonj@pixbyjade.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect('/thanks')
-
-    form = contactForm()
-    return render(request, 'index.html', {'form': form})
+    if request.method == 'GET':
+        return render(request, 'index.html')
 
 
 def thanks(request):
-    form = contactForm()
-    messages.success(request, 'Thank you! I will be reaching out ASAP')
-    return render(request, 'index.html', {'form': form})
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        return render(request, 'index.html', {'name': name})
+    else:
+        return render(request, 'index.html')
 
